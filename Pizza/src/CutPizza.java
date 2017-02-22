bcdd12e2161a144bb0e44c68ebfcec595b913452
kalpana <kalpana@192.168.1.110>
2017-02-22 15:00:23 +0100

    FindFactors

--- Pizza/src/CutPizza.java
+++ Pizza/src/CutPizza.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 //the helper class of cutting the pizza
 public class CutPizza {
 
 	public static Set<Shape> validShapes(Pizza pizza){
+		findFactors(pizza.getMaxSliceLength());
 		return null;
 	}
 	
@@ -11,5 +14,22 @@ public class CutPizza {
 		
 	}
 	
-	
+	public static List<Integer[]> findFactors(int num)
+    {
+        int incrementer = 1;
+        if (num % 2 != 0)
+        {
+            incrementer = 2; //only test the odd ones
+        }
+        List<Integer[]> list = new ArrayList<>();
+        for (int i = 1; i <= num / 2; i=i+incrementer)
+        {
+            if (num % i == 0)
+            {
+                list.add(new Integer[]{i, num/i});
+            }
+        }
+        list.add(new Integer[]{num,1});
+        return list;
+    }
 }
